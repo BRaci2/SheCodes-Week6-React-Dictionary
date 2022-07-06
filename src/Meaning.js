9d74ea5b460aa9276c68535711263d338570e605
B Raci <basia@live.ca>
2022-07-05 23:13:37 -0600

    working on Phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,20 +4,20 @@ import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
-    <div className="Meaning">
+    <span className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>;
       {props.meaning.definitions.map(function (definition, index) {
         return (
-          <div key={index}>
+          <span key={index}>
             <p>
               {definition.definition}
               <br />
               <em>{definition.example}</em>
               <Synonyms synonyms={definition.synonyms} />
             </p>
-          </div>
+          </span>
         );
       })}
-    </div>
+    </span>
   );
 }
